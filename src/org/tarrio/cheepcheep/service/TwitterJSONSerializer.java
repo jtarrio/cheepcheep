ef2f34a426494fadc8bf6e0cd0639973bd12dc1a
Jacobo Tarrio <jtarrio@gmail.com>
2009-12-13 14:58:39 +0000

    Parse server-side retweets especially so we get the whole message.

--- src/org/tarrio/cheepcheep/service/TwitterJSONSerializer.java
+++ src/org/tarrio/cheepcheep/service/TwitterJSONSerializer.java
@@ -61,7 +61,16 @@ public class TwitterJSONSerializer {
 		tweet.setInReplyToScreenName(getStringINN(status,
 				"in_reply_to_screen_name"));
 		tweet.setDateTime(parseDate(status.getString("created_at")));
-		tweet.setText(unescapeHtml(status.getString("text")));
+		if (status.has("retweeted_status")) {
+			JSONObject reTweet = status.getJSONObject("retweeted_status");
+			JSONObject reTweetAuthor = reTweet.getJSONObject("user");
+			StringBuilder sb = new StringBuilder("RT @");
+			sb.append(reTweetAuthor.getString("screen_name"));
+			sb.append(" ");
+			sb.append(reTweet.getString("text"));
+			tweet.setText(unescapeHtml(sb.toString()));
+		} else
+			tweet.setText(unescapeHtml(status.getString("text")));
 		JSONObject user = status.getJSONObject("user");
 		tweet.setScreenName(user.getString("screen_name"));
 		return tweet;
